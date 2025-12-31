fd3147235d313c4c13c0fc6432ca8246341777c3
Lee Matos <humans.find.me@leematos.com>
2025-12-30 23:10:10 -0500

    Simplify tab limit retrieval in background.js

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 const reactToNewTab = async tab => {
   const tabs = await browser.tabs.query({})
-  const res = await browser.storage.local.get('tab-limit')
-  const limit = res['tab-limit'] || browser.runtime.getManifest().DEFAULT_TAB_LIMIT
+  const limit = browser.runtime.getManifest().DEFAULT_TAB_LIMIT
   if (tabs.length > limit) await browser.tabs.remove(tab.id)
 }
 
